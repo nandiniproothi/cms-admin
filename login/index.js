099b82751cd260ae634d8105460e8cade03e9301
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-02-01 22:01:51 +0530

    👉🏾update👈🏾

--- login/index.js
+++ login/index.js
@@ -2,3 +2,35 @@ function forgotPassword(){
     document.getElementById("emailHelp").innerHTML = "please check your email!"
 }
 
+function googleSignIn() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user.displayName);
+        console.log(user.email);
+        console.log(user.uid);
+        if(user.email != "nandiniproothi@gmail.com" || user.email != ""){
+            console.log("sry");
+            window.location.replace("../sry.html");
+        }
+        //saving userid in localstorage so that we can use it any where
+        else{
+            window.location.replace("../dashboard/")
+        }
+
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
+//Signin with Google ends
